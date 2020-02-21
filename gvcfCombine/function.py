b743205bf42df0def70817d76f89d34305d6e305
Jin0331 <sempre813@hallym.ac.kr>
2020-02-21 15:59:51 +0900

    code update

--- gvcfCombine/function.py
+++ gvcfCombine/function.py
@@ -13,18 +13,9 @@ def hadoop_list(length, hdfs):
     proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     s_output, s_err = proc.communicate()
     all_dart_dirs = s_output.split()
-    
     return all_dart_dirs[:length]
 
-def hadoop_list(length, hdfs):
-    args = "hdfs dfs -ls "+ hdfs +" | awk '{print $8}'"
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    s_output, s_err = proc.communicate()
-    all_dart_dirs = s_output.split()
-    
-    return all_dart_dirs[:length]
-
-def preVCF(hdfs, flag, spark):
+def preVCF(hdfs, spark):
     vcf = spark.sparkContext.textFile(hdfs)
     #header_contig = vcf.filter(lambda x : re.match("^#", x))
     col_name = vcf.filter(lambda x : x.startswith("#CHROM")).first().split("\t")
@@ -32,14 +23,14 @@ def preVCF(hdfs, flag, spark):
                        .map(lambda x : x.split("\t"))\
                        .toDF(col_name)\
                        .withColumn("POS", F.col("POS").cast(IntegerType()))
+    sample_name = vcf_data.columns[-1]
+    vcf_data = vcf_data.drop("QUAL", "FILTER")
     
-    if flag == 1:
-        for index in range(len(vcf_data.columns) - 1):
-            compared_arr = ["#CHROM", "POS", "REF"]
-            if vcf_data.columns[index] in compared_arr:
-                continue
-            vcf_data = vcf_data.withColumnRenamed(vcf_data.columns[index], vcf_data.columns[index] + "_temp") 
-    
+    for index in range(len(vcf_data.columns) - 1):
+        compared_arr = ["#CHROM", "POS"]
+        if vcf_data.columns[index] in compared_arr:
+            continue
+        vcf_data = vcf_data.withColumnRenamed(vcf_data.columns[index], vcf_data.columns[index] + "_" + sample_name)     
     return vcf_data
 
 def chunks(lst, n):
@@ -53,6 +44,9 @@ def addIndex(POS, size):
         return int(POS / size + 1) 
 addIndex_udf = udf(addIndex, returnType=IntegerType())
 
+def unionAll(*dfs):
+    return reduce(DataFrame.unionByName, dfs) 
+
 # for indel
 word_len = udf(lambda col : True if len(col) >= 2 else False, returnType=BooleanType())
 ref_melt = udf(lambda ref : list(ref)[1:], ArrayType(StringType()))    
@@ -64,8 +58,23 @@ def ref_concat(temp):
     return return_str
 ref_concat = udf(ref_concat, ArrayType(StringType()))
 
-def unionAll(*dfs):
-    return reduce(DataFrame.unionByName, dfs) 
+def ref_max(left, right):
+    if left == None:
+        return right
+    elif right == None:
+        return left
+    else :
+        if len(left) >= len(right):
+            return left
+        else :
+            return right
+ref_max = udf(ref_max, StringType())
+
+def info_change(temp):
+    some_list = temp.split(";")
+    result = [i for i in some_list if i.startswith('DP=')]
+    return result[0]
+info_change = udf(info_change, StringType())
 
 # for sample value
 value_change = udf(lambda value : "./." + value[3:], StringType())
@@ -76,6 +85,104 @@ def sampling_func(data, ran):
     sample = data.take(range(0, N, ran))
     return sample
 
-def sample_join(left, right):
-    return left.select(F.col("#CHROM"), F.col("POS"), F.col("REF"), left.columns[-1])\
-        .join(right.select(F.col("#CHROM"), F.col("POS"), F.col("REF"), right.columns[-1]), ["#CHROM", "POS", "REF"], "full")
+def reduce_join(left, right):   
+    return_vcf = left.drop(left.columns[-1])\
+                     .join(right.drop(right.columns[-1]), ["#CHROM", "POS"], "full")
+    
+    return return_vcf
+
+def reduce_inner_join(left, right):   
+    return_vcf = left.join(right, ["#CHROM", "POS"], "inner")
+    
+    return return_vcf
+
+def column_name(df_col, name):
+    return_list = []
+    for col in df_col:
+        if col.startswith(name):
+            return_list.append(col)
+    return return_list
+
+def max_value(value):
+    value = list(filter(None, value))
+    if len(value) == 0:
+        return None
+    return max(value)
+max_value = udf(max_value, StringType())
+
+def info_min(value):
+    value = list(filter(None, value))
+    temp = [info for info in value if info.startswith("END=") == False]
+    temp = "%".join(temp)
+    
+    if temp == "":
+        return None
+    else :
+        return temp
+info_min = udf(info_min, StringType())
+
+def format_value(value): 
+    # ##FORMAT=<ID=SB,Number=4,Type=Integer,Description="Per-sample component statistics which comprise the Fisher's Exact Test to detect strand bias.">
+    value = list(filter(None, value))
+    if len(value) == 1:
+        value.append("GT:DP:GQ:MIN_DP:PL")
+    def format_reduce(left, right):
+        left, right = left.split(":"), right.split(":")
+        if len(left) <= len(right):        
+            temp = copy.deepcopy(right)
+            right = copy.deepcopy(left)
+            left = copy.deepcopy(temp)
+        for value in right:
+            if value not in left:
+                left.append(value)
+        return ":".join(left)
+    return str(reduce(format_reduce, value))
+format_value = udf(format_value, StringType())
+
+def with_vale(temp):
+    temp = temp.withColumn("REF", max_value(F.array(column_name(temp.columns, "REF"))))\
+     .drop(*column_name(temp.columns, "REF_"))\
+     .withColumn("ID", max_value(F.array(column_name(temp.columns, "ID"))))\
+     .drop(*column_name(temp.columns, "ID_"))\
+     .withColumn("ALT", max_value(F.array(column_name(temp.columns, "ALT"))))\
+     .drop(*column_name(temp.columns, "ALT_"))\
+     .withColumn("INFO", info_min(F.array(column_name(temp.columns, "INFO"))))\
+     .drop(*column_name(temp.columns, "INFO_"))\
+     .withColumn("FORMAT", F.array(column_name(temp.columns, "FORMAT")))\
+     .drop(*column_name(temp.columns, "FORMAT_"))
+    return temp
+
+def indel_union(temp):
+    temp = temp.filter(word_len(F.col("REF")))\
+            .withColumn("REF", ref_melt(F.col("REF"))).withColumn("REF", ref_concat(F.col("REF")))\
+            .withColumn("REF", explode(F.col("REF"))).withColumnRenamed("REF", "REF_temp")\
+            .withColumn('REF', split_col.getItem(0)).withColumn('POS_var', split_col.getItem(1))\
+            .drop(F.col("REF_temp")).withColumn("POS", (F.col("POS") + F.col("POS_var")).cast(IntegerType()))\
+            .drop(F.col("POS_var"))\
+            .withColumn('ID', F.lit("."))\
+            .withColumn('ALT', F.lit("*,<NON_REF>"))
+    return temp
+
+# 10개 기준임.
+def join_split(v_list):
+    stage1_list = list(chunks(v_list, 5))
+    if len(v_list) == 1:
+        return v_list    
+    stage1 = []
+    for vcf in stage1_list:
+        if len(vcf) == 1:
+            stage1.append(vcf)
+        else :
+            stage1.append(reduce(reduce_join, vcf))
+    return reduce(reduce_join, stage1)
+
+def join_split_inner(v_list):
+    stage1_list = list(chunks(v_list, 3))
+    stage1 = []
+    for vcf in stage1_list:
+        stage1.append(reduce(reduce_inner_join, vcf))
+    return stage1
+
+def find_duplicate(temp):
+    return temp.groupBy(F.col("#CHROM"), F.col("POS")).agg((F.count("*")>1).cast("int").alias("e"))\
+         .orderBy(F.col("e"), ascending=False)
--- gvcfCombine/main.py
+++ gvcfCombine/main.py
@@ -20,14 +20,13 @@ if __name__ == "__main__":
     spark = SparkSession.builder.master("spark://master:7077")\
                             .appName(appname)\
                             .config("spark.driver.memory", "8G")\
-                            .config("spark.driver.maxResultSize", "8G")\
-                            .config("spark.executor.memory", "24G")\
-                            .config("spark.executor.core", 3)\
-                            .config("spark.sql.execution.arrow.enabled", "false")\
-                            .config("spark.sql.execution.arrow.fallback.enabled", "false")\
+                            .config("spark.driver.maxResultSize", "5G")\
+                            .config("spark.executor.memory", "25G")\
+                            .config("spark.sql.execution.arrow.enabled", "true")\
+                            .config("spark.sql.execution.arrow.fallback.enabled", "true")\
                             .config("spark.network.timeout", "9999s")\
                             .config("spark.files.fetchTimeout", "9999s")\
-                            .config("spark.sql.shuffle.partitions", 40)\
+                            .config("spark.sql.shuffle.partitions", 80)\
                             .config("spark.eventLog.enabled", "true")\
                             .getOrCreate()
 
@@ -37,114 +36,82 @@ if __name__ == "__main__":
     from function import *
 
     # main
+    #gvcf_count = 20
     hdfs = "hdfs://master:9000"
     hdfs_list = hadoop_list(gvcf_count, "/raw_data/gvcf")
-
     vcf_list = list()
-    vcf_join_list = list()  
-    addIndex_udf = F.udf(addIndex, returnType=IntegerType())
-
-    # gVCF load
 
     for index in range(len(hdfs_list)):
-        if index == 0:
-            vcf_list.append(preVCF(hdfs + hdfs_list[index].decode("UTF-8"), 0, spark).cache())
-            inner_pos = vcf_list[index].select(F.col("#CHROM"), F.col("POS"), F.col("REF"))
-            info = vcf_list[index].select(vcf_list[index].columns[:9])
-        else:
-            vcf_list.append(preVCF(hdfs + hdfs_list[index].decode("UTF-8"), 1, spark).cache())
-            inner_pos_right = vcf_list[index].select(F.col("#CHROM"), F.col("POS"), F.col("REF"))
-            
-            if index % 2 != 0:
-                vcf_join_list.append(sample_join(vcf_list[index - 1], vcf_list[index]).cache())
-            if index % 2 == 0 and index == len(hdfs_list) - 1:
-                vcf_join_list.append(vcf_list[index].select(F.col("#CHROM"), F.col("POS"), F.col("REF"), vcf_list[index].columns[-1]).cache())
-            
-            # for column null value
-            info = info.join(vcf_list[index].select(vcf_list[index].columns[:9]), ["#CHROM", "POS", "REF"], "full")\
-                .withColumn("ID", when(F.col("ID").isNull(), F.col("ID_temp")).otherwise(F.col("ID")))\
-                .withColumn("ALT",when(F.col("ALT").isNull(), F.col("ALT_temp")).otherwise(F.col("ALT")))\
-                .withColumn("FORMAT", when(F.col("FORMAT").isNull(), F.col("FORMAT_temp")).otherwise(F.col("FORMAT")))\
-                .withColumn("QUAL", F.lit(".")).withColumn("FILTER", F.lit("."))\
-                .withColumn("INFO", when(F.col("INFO").startswith("END") == False, F.col("INFO"))\
-                            .when(F.col("INFO_temp").startswith("END") == False, F.col("INFO_temp")))\
-                .drop("INFO_temp", "ID_temp", "ALT_temp", "FORMAT_temp", "QUAL_temp", "FILTER_temp")
-            
-            # for index
-            inner_pos = inner_pos.join(inner_pos_right, ["#CHROM", "POS", "REF"], "inner")
+        vcf_list.append(preVCF(hdfs + hdfs_list[index].decode("UTF-8"), spark).cache())
 
-            if index == len(hdfs_list) - 1:
-                info = info.dropDuplicates()
-                inner_pos = inner_pos.dropDuplicates()      
+    if gvcf_count == 20 or gvcf_count == 15:
+        temp1 = join_split(vcf_list[:10])
+        temp2 = join_split(vcf_list[10:])
+        vcf = reduce(reduce_join, [temp1, temp2])
+    elif gvcf_count == 25:
+        temp1 = join_split(vcf_list[:10])
+        temp2 = join_split(vcf_list[10:20])
+        temp3 = join_split(vcf_list[20:])
+        temp4 = reduce(reduce_join, [temp1, temp2])
+        vcf = reduce(reduce_join, [temp3, temp4])
+    elif gvcf_count > 5 and gvcf_count <= 10:
+        temp1 = reduce(reduce_join, vcf_list[:5])
+        temp2 = reduce(reduce_join, vcf_list[5:])
+        vcf = reduce(reduce_join, [temp1, temp2])
+    elif gvcf_count <= 5:
+        vcf = reduce(reduce_join, vcf_list)
     
+    vcf = with_vale(vcf).cache()
+    vcf.count()    
+    print("full outer join with updated value!!\n")
+        
+    # info window
+    info_window = Window.partitionBy("#CHROM").orderBy("POS")
+    vcf_not_indel = vcf.withColumn("INFO", when(F.col("INFO").isNull(), F.concat(F.lit("END="), F.lead("POS", 1).over(info_window) - 1))\
+                                .otherwise(F.col("INFO")))
+
+    # dropduplicates할 때, indel 삭제되는 경우 있음.
     # indel union
     split_col = F.split("REF_temp", '_')
-    indel = info.filter(word_len(F.col("REF")))\
-                .withColumn("REF", ref_melt(F.col("REF"))).withColumn("REF", ref_concat(F.col("REF")))\
-                .withColumn("REF", explode(F.col("REF"))).withColumnRenamed("REF", "REF_temp")\
-                .withColumn('REF', split_col.getItem(0)).withColumn('POS_var', split_col.getItem(1))\
-                .drop(F.col("REF_temp")).withColumn("POS", (F.col("POS") + F.col("POS_var")).cast(IntegerType()))\
-                .drop(F.col("POS_var"))\
-                .withColumn('ID', F.lit("."))\
-                .withColumn('ALT', F.lit("*,<NON_REF>"))
+    indel = indel_union(vcf)
 
-    # window
-    info_window = Window.partitionBy("#CHROM").orderBy("POS")
-    info = info.withColumn("INFO", when(F.col("INFO").isNull(), F.concat(F.lit("END="), F.lead("POS", 1).over(info_window) - 1))\
-                                .otherwise(F.col("INFO")))\
-            .orderBy(F.col("#CHROM"), F.col("POS"))
+    indel_com = unionAll(*[indel, vcf_not_indel])\
+                    .orderBy(F.col("#CHROM"), F.col("POS"))\
+                    .dropDuplicates(["#CHROM", "POS"])\
+                    .cache()                 
+    indel_com.count()
 
-    info = unionAll(*[info, indel]).orderBy(F.col("#CHROM"), F.col("POS")).cache()
-    info.count()
+    indel.unpersist()
+    vcf.unpersist()
+    print("indel add completed!!\n")
 
-    # pos_index
-    inner_pos = spark.createDataFrame(inner_pos.drop(F.col("REF")).orderBy(F.col("#CHROM"), F.col("POS"))\
-                .toPandas().groupby("#CHROM", group_keys=False).apply(sampling_func, ran = 13).sort_index())\
-                .withColumnRenamed("#CHROM", "chr_temp")\
-                .withColumnRenamed("POS", "pos_temp")\
-                .orderBy(F.col("#CHROM"), F.col("POS")).cache()
-    inner_pos.count()
-    print("gVCF lode & join (inner, full) done@")
+    # parquet write
+    sample_w = Window.partitionBy(F.col("#CHROM")).orderBy(F.col("POS")).rangeBetween(Window.unboundedPreceding, Window.currentRow)  
+    parquet_list = list()
+    cnt = 0
 
-    pos_index = Window.partitionBy("#CHROM").orderBy("POS").rangeBetween(Window.unboundedPreceding, Window.currentRow)
-    ex = [info["#CHROM"] == inner_pos["chr_temp"], info["POS"] == inner_pos["pos_temp"]]
-    temp = info.select(F.col("#CHROM"), F.col("POS")).join(inner_pos, ex, "full")\
-            .drop(F.col("chr_temp"))\
-            .dropDuplicates()\
-            .withColumn("POS_INDEX", when(F.col("pos_temp").isNull(), F.last(F.col("pos_temp"), ignorenulls=True).over(pos_index))\
-                            .otherwise(F.col("pos_temp")))\
-            .drop(F.col("pos_temp")).orderBy(F.col("#CHROM"), F.col("POS"))
+    ## sample & info comibne, value window
+    for index in range(len(vcf_list)):
+        temp = indel_com.select(["#CHROM","POS"])\
+            .join(vcf_list[index].select(["#CHROM", "POS"] + vcf_list[index].columns[7:]), ["#CHROM", "POS"], "full")
+            # sample window    
+        temp = temp.withColumn(temp.columns[-1], F.last(temp.columns[-1], ignorenulls=True).over(sample_w))
+        parquet_list.append(temp)
 
-    info_index = info.join(temp, ["#CHROM", "POS"], "inner").orderBy(F.col("#CHROM"), F.col("POS")).dropDuplicates()\
-                    .repartition(F.col("#CHROM"), F.col("POS_INDEX")).cache()
+    ## parquet write
+    for parquet in join_split_inner(parquet_list):
+        time.sleep(30)
+        parquet.write.mode('overwrite')\
+               .parquet("/raw_data/output/gvcf_output/"+ folder_name + "//" + "sample_" + str(cnt) + ".g.vcf")
+            
+        for index in range(cnt, cnt + 3):
+            if len(vcf_list) - 1 < index:
+                break
+            vcf_list[index].unpersist()
+        cnt += 3
+        print("sample" + str(cnt - 1) + "done !!")
 
-    info_index.count()
-    info.unpersist()
-    print("POS_INDEX done!")
-
-    # sample window & parquet write
-    vcf_list_pos_index = list()
-    sample_w = Window.partitionBy(F.col("#CHROM"), F.col("POS_INDEX")).orderBy(F.col("POS")).rangeBetween(Window.unboundedPreceding, Window.currentRow)  
-
-    for index in range(len(vcf_join_list)):
-        temp = info_index.select(["#CHROM","POS","REF","POS_INDEX"])\
-            .join(vcf_join_list[index].select(["#CHROM", "POS", "REF"] + vcf_join_list[index].columns[3:]), ["#CHROM", "POS", "REF"], "full")
+    indel_com.write.mode('overwrite')\
+                    .parquet("/raw_data/output/gvcf_output/" + folder_name + "//info.g.vcf")
         
-        # sample window
-        for sample_name in temp.columns[4:]:     
-            temp = temp.withColumn(sample_name, when(F.col(sample_name).isNull(), F.last(sample_name, ignorenulls=True).over(sample_w))\
-                                                    .otherwise(F.col(sample_name)))\
-                        .withColumn(sample_name, value_change(F.col(sample_name)))
-        vcf_list_pos_index.append(temp)
-        
-    for write_parquet in vcf_list_pos_index:
-        write_parquet.drop(F.col("POS_INDEX"))\
-                    .write.partitionBy("#CHROM")\
-                    .mode('overwrite')\
-                    .parquet("/raw_data/output/gvcf_output/"+ folder_name + "//" + "_".join(write_parquet.columns[4:]) + ".g.vcf")
-        
-    info_index.drop(F.col("POS_INDEX"))\
-            .write.partitionBy("#CHROM").mode('overwrite')\
-            .parquet("/raw_data/output/gvcf_output/" + folder_name + "//info.g.vcf")
-    
-    print("paquet write done!!")
+            #.withColumn(sample_name, value_change(F.col(sample_name)))
